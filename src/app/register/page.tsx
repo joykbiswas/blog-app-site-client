0aa991597aa2e70d3e7b1c7832d5a7e08d3850ab
joykbiswas <joykumarbiswas100@gmail.com>
2026-01-13 15:11:59 +0600

    routing pag create

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegistrationPage = () => {
+    return (
+        <div>
+            <h1>This is Reg page</h1>
+        </div>
+    );
+};
+
+export default RegistrationPage;
